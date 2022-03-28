44199ec846b745f066f33067f54c81f1553baf55
Yoonjungoh <arcnature@naver.com>
2022-03-28 22:20:47 +0900

    채팅메세지 작업-1

--- DummyClient/Packet/PacketHandler.cs
+++ DummyClient/Packet/PacketHandler.cs
@@ -5,8 +5,16 @@ using System.Text;
 
 class PacketHandler
 {
-	public static void S_TestHandler(PacketSession session, IPacket packet)
+	public static void C_ChatHandler(PacketSession session, IPacket packet)
 	{
+		//C_Chat chatPacket = packet as C_Chat;
+		//ClientSession clientSession = session as clientSession
 
+  //      Console.WriteLine(($"PlayerInfoReq: {chatPacket.playerId} {chatPacket.name}");
+
+		//foreach(C_PlayerInfoReq.Skill skill in chatPacket.skills)
+  //      {
+  //          Console.WriteLine($"Skill({skill.id}) ({skill.level}) ({skill.duration})");
+  //      }
 	}
 }
