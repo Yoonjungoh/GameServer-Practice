0f4489a9bc7e78f360a270e60daffa929baf5faa
Yoonjungoh <arcnature@naver.com>
2022-04-07 01:57:49 +0900

    jonTimer

--- Server/Program.cs
+++ Server/Program.cs
@@ -26,10 +26,15 @@ namespace Server
 			_listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
 			Console.WriteLine("Listening...");
 
+			int roomTick = 0;
 			while (true)
 			{
-				Room.Push(() => Room.Flush());
-				Thread.Sleep(250);
+				int now = System.Environment.TickCount;
+				if(roomTick < now)
+				{
+					Room.Push(() => Room.Flush());
+					roomTick = now + 250;
+				}
 			}
 		}
 	}
