4c40687fb706084690db83631cf89649ab7f9c66
Yoonjungoh <arcnature@naver.com>
2022-04-07 01:49:36 +0900

    commit

--- Server/GameRoom.cs
+++ Server/GameRoom.cs
@@ -9,11 +9,21 @@ namespace Server
     {
         List<ClientSession> _sessions = new List<ClientSession>();
         JobQueue _jobQueue = new JobQueue();
+        List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
+
 
 
         public void Push(Action job)
         {
             _jobQueue.Push(job);
+        }
+        public void Flush()
+        {
+            foreach (ClientSession s in _sessions)   // 패킷을 요청이 올때마다 보내는게 아니라 뭉쳐서 보내는게 해결책인듯!!(N^2에서 N으로 줄일 수 있음)
+                s.Send(_pendingList);
+
+            Console.WriteLine($"Flushed {_pendingList.Count} items");
+            _pendingList.Clear();
         }
         public void Broadcast(ClientSession session, string chat)
         {
@@ -22,8 +32,8 @@ namespace Server
             packet.chat = $"{chat} I am {packet.playerId}";
             ArraySegment<byte> segment = packet.Write();
 
-            foreach(ClientSession s in _sessions)   // 패킷을 요청이 올때마다 보내는게 아니라 뭉쳐서 보내는게 해결책인듯!!(N^2에서 N으로 줄일 수 있음)
-                s.Send(segment);
+            _pendingList.Add(segment);
+
         }
         public void Enter(ClientSession session)
         {
